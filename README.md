d672a875e7d921c5fbfe915093fa3395ee654041
Чеблуков Андрей Александрович <Cheblukov.A.A@sberbank.ru>
2020-08-25 13:02:16 +0300

    Updates

--- README.md
+++
@@ -1,7 +0,0 @@
-This app provides searching songs by artists and filters it by album and/or name of a song.
-
-Technologies: React v.16.8.2, Redux v.4.0.4 and React-Redux v.7.1.1. In near future, I am going to add TypeScript v.3.5 or above.
-
-Dependecies requirement: I developed this project with node v.10.16.3 and yarn 1.17.3.
-
-To build it properly, please, make sure, that you have installed same version of this packages.
---
+++ precache-manifest.e24d15806b12c43554cecc3bb67a85c5.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "579061df9541653cb6bd",
+    "url": "/itunes-search/tree/master/static/css/main.15bfd541.chunk.css"
+  },
+  {
+    "revision": "579061df9541653cb6bd",
+    "url": "/itunes-search/tree/master/static/js/main.579061df.chunk.js"
+  },
+  {
+    "revision": "d02e31dd832b7141c06b",
+    "url": "/itunes-search/tree/master/static/js/runtime~main.d02e31dd.js"
+  },
+  {
+    "revision": "cd2c6a5b9078cd10f21b",
+    "url": "/itunes-search/tree/master/static/css/2.54083c45.chunk.css"
+  },
+  {
+    "revision": "cd2c6a5b9078cd10f21b",
+    "url": "/itunes-search/tree/master/static/js/2.cd2c6a5b.chunk.js"
+  },
+  {
+    "revision": "45971bb981e87188de174f1040b80aba",
+    "url": "/itunes-search/tree/master/static/media/play.45971bb9.svg"
+  },
+  {
+    "revision": "fea842c0926f71e7237a6179b2d276b0",
+    "url": "/itunes-search/tree/master/static/media/pause.fea842c0.svg"
+  },
+  {
+    "revision": "7bc8a7dcc77d4d6f79ed0032954a6a6a",
+    "url": "/itunes-search/tree/master/index.html"
+  }
+];
